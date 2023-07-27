094af776ed49e6a14dca74d32d6e87f91c4752ef
mandudiego <d.mandubarreiro@gmail.com>
2023-07-26 21:20:51 -0300

    Classes implementadas: Board, Piece

--- src/board/Piece.java
+++ src/board/Piece.java
@@ -1,5 +1,19 @@
 package board;
 
-public class Piece {
+public class Piece{
+	protected Position position;
+	private Board board;
+	
+	public Piece() {
+	}
 
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+	
 }
