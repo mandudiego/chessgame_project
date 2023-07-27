949308e9834818ea2fbdf723a3312f2ece1b321a
mandudiego <d.mandubarreiro@gmail.com>
2023-07-26 21:54:07 -0300

    Implementando classes: UI, ChessMatch, ChessPiece

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -1,5 +1,32 @@
 package chess;
 
-public class ChessPiece {
+import board.Board;
+import board.Piece;
 
+public class ChessPiece extends Piece{
+	
+	private Color color;
+	private int moveCount;
+	
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public int getMoveCount() {
+		return moveCount;
+	}
+
+	public void setMoveCount(int moveCount) {
+		this.moveCount = moveCount;
+	}
+	
 }
